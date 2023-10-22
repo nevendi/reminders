cbbae302cccb09b77152c5ae92447fab2f045293
Adderly  Jáuregui Campos <emmat@MacBook-Pro-de-Adderly.local>
2023-10-21 20:41:52 -0600

    Migrating to generic controllers and services

--- src/messages/messages_templates/messages_templates.controller.ts
+++ src/messages/messages_templates/messages_templates.controller.ts
@@ -3,11 +3,13 @@ import { MessagesTemplatesService } from './messages_templates.service';
 import { CreateMessageTemplateDto } from '../dto/create_message_template.dto';
 import { BaseController } from '../../common/base.controller';
 import { MessageTemplate } from '../entities/message_template.entity';
+import { Message } from '../entities/message.entity';
 
 @Controller('messages/templates')
-export class MessagesTemplatesController{
+export class MessagesTemplatesController extends BaseController<MessageTemplate> {
   constructor(
     private readonly messageTemplatesService: MessagesTemplatesService,
   ) {
+    super(messageTemplatesService);
   }
 }
--- src/messages/messages_templates/messages_templates.service.ts
+++ src/messages/messages_templates/messages_templates.service.ts
@@ -4,28 +4,29 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { MessageTemplate } from '../entities/message_template.entity';
 import { Repository } from 'typeorm';
 import { CustomersService } from '../../users/customers/customers.service';
+import { Service } from '../../common/service';
 
 @Injectable()
-export class MessagesTemplatesService{
+export class MessagesTemplatesService extends Service<MessageTemplate> {
   constructor(
     @InjectRepository(MessageTemplate)
     private readonly messageTemplateRepository: Repository<MessageTemplate>,
     private readonly customersService: CustomersService,
   ) {
-
+    super(messageTemplateRepository);
   }
 
-  async create(
-    createTemplateDto: CreateMessageTemplateDto,
-  ): Promise<MessageTemplate> {
+  async create(createTemplateDto: CreateMessageTemplateDto): Promise<string> {
     console.log(createTemplateDto);
     const customer = await this.customersService.findOne(
-      'cea295c1-78a4-4224-b68d-ce6df6d4ec2b',
+      '09319231-49f4-4d86-8fb2-b3df6a0f1a8b',
     );
     const messageTemplate = this.messageTemplateRepository.create({
       ...createTemplateDto,
       customer,
     });
-    return await this.messageTemplateRepository.save(messageTemplate);
+    const newMessageTemplate =
+      await this.messageTemplateRepository.save(messageTemplate);
+    return newMessageTemplate.id;
   }
 }
